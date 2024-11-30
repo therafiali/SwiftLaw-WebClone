2dac1c18461df8e4a57d7327b166960e1ac6025a
Rafi Ali <therafiali@gmail.com>
2024-11-30 15:40:25 +0500

    fixed navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,35 +9,45 @@ import MyButton from '../shared/MyButton'
 import myLogo from '@/assets/Logo/ym_l.png'
 import Image from 'next/image'
 
-
 export default function NavbarApp () {
   return (
-    <Navbar className='flex justify-between px-8 my-4' isBordered>
-      {/* Logo */}
-      <NavbarBrand>
-        <Image src={myLogo} alt='YourMunshi' width={150} height={150} />
-      </NavbarBrand>
-      <div className='flex  space-x-8'>
-        {/* options */}
-        <NavbarContent className='hidden sm:flex gap-4 font-medium' justify='center'>
-          <NavbarItem>
-            <Link color='foreground' href='#'>
-              Pricing
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href='#' aria-current='page'>
-              Blog
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        {/* Button */}
-        <NavbarContent justify='end'>
-          <NavbarItem>
-            <MyButton button_text='Start Now' />
-          </NavbarItem>
-        </NavbarContent>
-      </div>
-    </Navbar>
+    <>
+      <Navbar
+        className='
+        fixed top-0 left-0 right-0 z-10 flex justify-between items-center px-8 py-4 
+        backdrop-blur-md bg-white/30 border border-white/40 '
+        isBordered
+      >
+        {/* Logo */}
+        <NavbarBrand>
+          <Image src={myLogo} alt='YourMunshi' width={150} height={150} />
+        </NavbarBrand>
+        <div className='flex items-center space-x-8 '>
+          {/* options  */}
+          <NavbarContent
+            className='hidden sm:flex gap-4 font-medium'
+            justify='center'
+          >
+            <NavbarItem>
+              <Link color='foreground' href='#'>
+                Pricing
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href='#' aria-current='page'>
+                Blog
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          {/* Button */}
+          <NavbarContent justify='end'>
+            <NavbarItem>
+              <MyButton button_text='Start Now' />
+            </NavbarItem>
+          </NavbarContent>
+        </div>
+      </Navbar>
+      <hr className='absolute w-full bottom-0 transition-opacity duration-300 ease-in-out opacity-0' />
+    </>
   )
 }
