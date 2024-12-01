33a4eabfd22a344d9094038da373f9dea598750e
Rafi Ali <therafiali@gmail.com>
2024-12-01 14:44:08 +0500

    correct partners sections

--- src/components/Patners/MyPartners.tsx
+++ src/components/Patners/MyPartners.tsx
@@ -4,13 +4,74 @@ const MyPartners = () => {
   return (
     <section className=''>
       <div className='container mx-auto px-4 md:px-8 py-12'>
-
-      
-        <h3 className='text-center text-sm font-semibold text-gray-500 '>
+  <h3 className='text-center text-sm font-semibold text-gray-500 '>
           TRUSTED BY FIRMS ACROSS THE WORLD
         </h3>
         <div className='relative mt-6'>
           <div className='group flex overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row max-w-full [--duration:40s]'>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
+            <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
+              <img
+                className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
+                src='https://www.tryswiftlaw.com/_next/image?url=%2Fcustomers%2Fbenemeritolaw.png&w=256&q=75'
+                alt=''
+              />
+            </div>
             <div className='flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row'>
               <img
                 className='h-10 w-28 dark:brightness-0 dark:invert grayscale opacity-30 mx-4'
