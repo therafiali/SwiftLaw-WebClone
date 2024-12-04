7fc47bf1ae3a803d923e52cb65aeb916d902e023
Rafi Ali <therafiali@gmail.com>
2024-12-04 16:11:53 +0500

    add navigation button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react'
 import {
   Navbar,
   NavbarBrand,
@@ -8,8 +10,22 @@ import Link from 'next/link'
 import MyButton from '../shared/MyButton'
 import myLogo from '@/assets/Logo/ym_l.png'
 import Image from 'next/image'
+import { X } from 'lucide-react'
 
 export default function NavbarApp () {
+  // State to toggle the mobile menu
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+
+  // Function to close the mobile menu
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false)
+  }
+
   return (
     <>
       <Navbar
@@ -22,11 +38,12 @@ export default function NavbarApp () {
         <NavbarBrand>
           <Image src={myLogo} alt='YourMunshi' width={150} height={150} />
         </NavbarBrand>
-        <div className='flex items-center space-x-8 '>
-          {/* options  */}
+
+        <div className='flex items-end justify-end w-full px-8  space-x-8'>
+          {/* options for large screens */}
           <NavbarContent
-            className='hidden sm:flex gap-4 font-medium'
-            justify='center'
+            className='hidden sm:flex  gap-4 font-medium'
+            justify='end'
           >
             <NavbarItem>
               <Link color='foreground' href='#'>
@@ -39,15 +56,56 @@ export default function NavbarApp () {
               </Link>
             </NavbarItem>
           </NavbarContent>
-          {/* Button */}
-          <NavbarContent justify='end'>
+        </div>
+
+        <div className='flex space-x-4'>
+          <NavbarContent justify='end' className='flex'>
             <NavbarItem>
               <MyButton button_text='Login / Signup' />
             </NavbarItem>
           </NavbarContent>
+
+          <div className='sm:hidden flex items-center'>
+            <button
+              onClick={toggleMobileMenu}
+              className='p-2 text-gray-600 hover:text-gray-800 focus:outline-none'
+            >
+              <span className='block w-6 h-0.5 bg-gray-800 mb-1'></span>
+              <span className='block w-6 h-0.5 bg-gray-800 mb-1'></span>
+              <span className='block w-6 h-0.5 bg-gray-800'></span>
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={`sm:hidden fixed top-0 left-0 w-full backdrop-blur-sm bg-white/95 border border-sla z-40 
+            transform transition-all duration-300 ease-in-out 
+            ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
+        >
+          <div className='relative mt-6'>
+            {/* Close Button */}
+            <button
+              onClick={closeMobileMenu}
+              className='absolute top-0 right-5 p-2 text-primary'
+            >
+              <X />
+            </button>
+
+            {/* Mobile Menu Content */}
+            <div className='flex flex-col text-primary items-center space-y-4 py-6 '>
+              <NavbarItem>
+                <Link href='#'>Pricing</Link>
+              </NavbarItem>
+              <NavbarItem isActive>
+                <Link href='/blog' aria-current='page'>
+                  Blog
+                </Link>
+              </NavbarItem>
+            </div>
+          </div>
         </div>
       </Navbar>
-      <hr className='absolute w-full bottom-0 transition-opacity duration-300 ease-in-out opacity-0' />
     </>
   )
 }
