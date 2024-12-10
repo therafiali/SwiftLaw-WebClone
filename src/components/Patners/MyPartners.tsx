283b446a4e4ebcb0ef292ff07eb6dfb4bbef5fdd
Rafi Ali <therafiali@gmail.com>
2024-12-10 18:59:00 +0500

    add changes in patners section

--- src/components/Patners/MyPartners.tsx
+++ src/components/Patners/MyPartners.tsx
@@ -12,63 +12,47 @@ const MyPartners = () => {
           TRUSTED BY OUR PARTNERS LAW FIRMS
         </h3>
         <div className="relative mt-6">
-          <div className="group flex mx-auto items-center justify-center overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row max-w-full [--duration:40s]">
+          <div className="group flex mx-auto items-center justify-center space-x-4 overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row max-w-full [--duration:40s]">
+
             <div>
               <Image
-                className=" w-28 h-24"
+                className=""
                 src={commingSoon}
                 alt="mufti"
-                width={2000}
-                height={2000}
-              />
-            </div>
-            <div>
-              <Image
-                className="w-24 mx-4"
-                src={commingSoon}
-                alt="mufti"
-                width={2000}
-                height={2000}
+                width={150}
+                height={150}
               />
             </div>
 
             <div className="">
               <Image
-                className="w-24 mx-4"
+                className=""
                 src={mufti}
                 alt="mufti"
-                width={2000}
-                height={2000}
+                width={300}
+                height={300}
               />
             </div>
 
             <div>
               <Image
-                className="w-24 mx-4"
+                className=""
                 src={khatans}
                 alt="mufti"
-                width={2000}
-                height={2000}
+                width={200}
+                height={200}
               />
             </div>
             <div>
               <Image
-                className="w-24 mx-4"
+                className=""
                 src={commingSoon}
                 alt="mufti"
-                width={2000}
-                height={2000}
+                width={150}
+                height={150}
               />
             </div>
-            <div>
-              <Image
-                className="w-24 mx-4"
-                src={commingSoon}
-                alt="mufti"
-                width={2000}
-                height={2000}
-              />
-            </div>
+
           </div>
           <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-background"></div>
           <div className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-background"></div>
