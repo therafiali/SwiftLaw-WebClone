f7bc3484d17defa58deaeb980b4874ead72d450d
Rafi Ali <therafiali@gmail.com>
2024-12-12 10:57:38 +0500

    change pricing features

--- src/components/Pricing/PricingSection.tsx
+++ src/components/Pricing/PricingSection.tsx
@@ -25,29 +25,29 @@ const BasicPlan = [
   { text: 'Judgement copy downloads', value: false },
   { text: 'Case lead forwarding', value: false },
   { text: 'Support', value: false },
-  { text: 'Access to LegalERP', value: false },
+  // { text: 'Access to LegalERP', value: false },
   { text: 'YM Club Membership', value: false }
 ]
 
 const StandardPlan = [
   { text: 'Case Law research', value: true },
   { text: 'Citations & summaries', value: true },
-  { text: 'Limited legal drafting', value: true },
-  { text: 'Limited judgement copies', value: true },
+  { text: 'Legal Drafting (Limited)', value: true },
+  { text: 'Judgement Copies (Limited) ', value: true },
   { text: 'Email support', value: true },
   { text: 'Case lead forwarding', value: false },
-  { text: 'Access to LegalERP', value: false },
+  // { text: 'Access to LegalERP', value: false },
   { text: 'YM Club Membership', value: false }
 ]
 
 const PremiumPlan = [
-  { text: 'Unlimited Case law research', value: true },
-  { text: 'Unlimited citations & copies', value: true },
-  { text: 'Unlimited legal drafting', value: true },
+   { text: 'Case Law research (Unlimited)', value: true },
+  { text: 'Citations & Copies (Unlimited)', value: true },
+  { text: 'Legal Drafting (Unlimited)', value: true },
   { text: 'Judgement copy downloads', value: true },
   { text: 'Case lead forwarding', value: true },
   { text: 'In-person support', value: true },
-  { text: 'Discounted rates for LegalERP', value: true },
+  // { text: 'Discounted rates for LegalERP', value: true },
   { text: 'Exclusive YM club membership', value: true }
 ]
 
